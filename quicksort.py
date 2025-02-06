3e24f65313f2bf9deb44d16ee0e8731a5185f93f
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2025-02-05 23:03:31 -0300

    algoritmo quicksort

---
+++ quicksort.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivo = array[0]
+        menores = [i for i in array[1:] if i <= pivo]
+        maiores = [i for i in array[1:] if i > pivo]
+        return quicksort(menores) + [pivo] + quicksort(maiores)
+
+
+class TestQuicksort(unittest.TestCase):
+
+    def test_quicksort(self):
+        lista = [20, 1, 3, 50, 60]
+        resposta = quicksort(lista)
+        expectativa = [1, 3, 20, 50, 60]
+        assert resposta == expectativa
